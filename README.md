5cb8ff2574b88a738f56c560b7ed551a5385e8c8
Eddie Hillenbrand <eddie.hillenbrand@adcolony.com>
2016-03-15 13:08:52 -0700

    Add note about origin of the original source.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # libevent-echo-server
 Simple libevent echo server
+
+This is a modified version of the "simpler ROT13 server with Libevent"
+from: http://www.wangafu.net/~nickm/libevent-book/01_intro.html
--- echo-server.c
+++ echo-server.c
@@ -1,5 +1,9 @@
 /*
  * echo-server.c
+ *
+ * This is a modified version of the "simpler ROT13 server with
+ * Libevent" from:
+ * http://www.wangafu.net/~nickm/libevent-book/01_intro.html
  */
 
 #include <netinet/in.h>
